7b5635a18a9443dd70243bedbc2b25b848f2ca46
MedyOsef <porgomedyfaycal@gmail.com>
2026-04-14 11:19:58 +0000

    Update greeting name in MainActivity
    - Change the default greeting name from "Android" to "Medy".

--- app/src/main/java/com/example/fakelocation/MainActivity.kt
+++ app/src/main/java/com/example/fakelocation/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FakeLocationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Medy",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
